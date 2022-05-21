c0cea6b3c70148ad7babf459c4d88b90334c9952
0nhc <60451384+0nhc@users.noreply.github.com>
2022-05-22 04:12:59 +0800

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -1,6 +1,6 @@
 sudo docker run -id --name ros_noetic --network host \
 	--privileged -v /dev:/dev -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 \
 	-v /sys/class/pwm:/sys/class/pwm \
-	-v /sys/class/gpio:/sys/class/gpio \
+	-v /sys/bus/i2c/devices/3-0050/eeprom:/sys/bus/i2c/devices/3-0050/eeprom \
 	0nhc/mnpp:test
 sudo xhost +
